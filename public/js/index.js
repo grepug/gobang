a388c959a027328c5aaf63663b52b3c067d47fb3
grepug <sk@grepug.com>
2015-04-20 23:40:27 +0800

    add chat feature

---
+++ public/js/index.js
@@ -0,0 +1,125 @@
+$(function () {
+
+  var role = 'white';
+  var versus = [];
+  for (var i = 0; i < 15; i++) {
+    versus[i] = [];
+    for (var j = 0; j < 15; j++)
+      versus[i][j] = null;
+  }
+  $('table').html(createTable());
+  $(".empty").text('`').on('click', function () {
+    role = role == 'white' ? 'black' : 'white';
+    var $this = $(this),
+      x = $this.data('x'),
+      y = $this.data('y');
+    $this.replaceWith("<img class='pawn' data-x='" + x + "' data-y='" + y + "' src='img/" + role + ".jpg'>");
+    versus[x][y] = role;
+    console.log(versus);
+    var win = isWin();
+    if (win) {
+      console.log(role + " win!");
+      console.log(win);
+      for (var i = 0; i < win.length; i++) {
+        $("[data-x='" + win[i][0] + "'][data-y='" + win[i][1] + "']").attr('src', 'img/' + role + '-win.jpg');
+        console.log(win[i][0])
+      }
+      //location.reload();
+    }
+  });
+
+  function isWin() {
+
+    for (var i = 0; i < 15; i++) {
+      for (var j = 0; j < 11; j++) {
+        var winArr = straightEqual(false, '+', i, j)
+        if (winArr) return winArr;
+      }
+    }
+
+    for (var i = 0; i < 15; i++) {
+      for (var j = 0; j < 11; j++) {
+        var winArr = straightEqual('+', false, i, j)
+        if (winArr) return winArr;
+      }
+    }
+
+    for (var i = 0; i < 14; i++) {
+
+      for (var j = 0; j < 14; j++) {
+
+        if (i <= 10 && j <= 10) {
+          var winArr = straightEqual('+', '+', i, j)
+          if (winArr) return winArr;
+        }
+        if (i >= 4 && j >= 4) {
+          var winArr = straightEqual('-', '-', i, j)
+          if (winArr) return winArr;
+        }
+        if (i >= 4 && j <= 10) {
+          var winArr = straightEqual('-', '+', i, j)
+          if (winArr) return winArr;
+        }
+        if (i <= 10 && j >= 4) {
+          var winArr = straightEqual('+', '-', i, j)
+          if (winArr) return winArr;
+        }
+      }
+    }
+    return false;
+  }
+
+  function straightEqual(m, m1, _i, _j) {
+    if (versus[_i][_j] == null) return false;
+    var winArr = [[_i, _j]];
+    for (var i = 1; i <= 4; i++) {
+
+      if (!m && m1 == '+') {
+        if (versus[_i][_j] != versus[_i][_j + i]) return false;
+        winArr.push([_i, _j + i]);
+      }
+      if (!m && m1 == '-') {
+        if (versus[_i][_j] != versus[_i][_j - i]) return false;
+        winArr.push([_i, _j - i]);
+      }
+      if (m == '+' && !m1) {
+        if (versus[_i][_j] != versus[_i + i][_j]) return false;
+        winArr.push([_i + i, _j]);
+      }
+      if (m == '+' && m1 == '+') {
+        if (versus[_i][_j] != versus[_i + i][_j + i]) return false;
+        winArr.push([_i + i, _j + i]);
+      }
+      if (m == '+' && m1 == '-') {
+        if (versus[_i][_j] != versus[_i + i][_j - i]) return false;
+        winArr.push([_i + i, _j - i]);
+      }
+      if (m == '-' && !m1) {
+        if (versus[_i][_j] != versus[_i - i][_j]) return false;
+        winArr.push([_i - i, _j]);
+      }
+      if (m == '-' && m1 == '+') {
+        if (versus[_i][_j] != versus[_i - i][_j + i]) return false;
+        winArr.push([_i - i, _j + i]);
+      }
+      if (m == '-' && m1 == '-') {
+        if (versus[_i][_j] != versus[_i - i][_j - i]) return false;
+        winArr.push([_i - i, _j - i]);
+      }
+
+    }
+    return winArr;
+  }
+});
+
+function createTable() {
+  var ret = '';
+  for (i = 0; i < 15; i++) {
+    ret += '<tr>';
+    for (j = 0; j < 15; j++) {
+      ret += '<td><a class="pawn empty" data-x="' + i + '" data-y="' + j + '"></td>';
+    }
+    ret += '</tr>';
+  }
+  return ret;
+}
---
+++ public/js/realtime.js
@@ -0,0 +1,183 @@
+// 请将 AppId 改为你自己的 AppId，否则无法本地测试
+var appId = '3l36nhyvwa2gg80zqq14lkkyhmmx1ukt7t89yg0byzyfqv4m';
+
+// 请换成你自己的一个房间的 conversation id
+var roomId = '55351818e4b0825685f6d7b8';
+
+// 每个客户端自定义的 id
+var clientId = 'mygobang';
+
+var rt;
+var room;
+
+// 监听是否服务器连接成功
+var firstFlag = true;
+
+var openBtn = document.getElementById('open-btn');
+var sendBtn = document.getElementById('send-btn');
+var inputName = document.getElementById('input-name');
+var inputSend = document.getElementById('input-send');
+var printWall = document.getElementById('print-wall');
+
+bindEvent(openBtn, 'click', main);
+bindEvent(sendBtn, 'click', sendMsg);
+
+bindEvent(document.body, 'keydown', function (e) {
+  if (e.keyCode === 13) {
+    if (firstFlag) {
+      main();
+    } else {
+      sendMsg();
+    }
+  }
+});
+
+function main() {
+  showLog('正在链接服务器，请等待。。。');
+  var val = inputName.value;
+  if (val) {
+    clientId = val;
+  }
+  if (!firstFlag) {
+    rt.close();
+  }
+
+  // 创建实时通信实例
+  rt = AV.realtime({
+    appId: appId,
+    clientId: clientId
+  });
+
+  // 监听连接成功事件
+  rt.on('open', function () {
+    firstFlag = false;
+    showLog('服务器连接成功！');
+
+    // 获得已有房间的实例
+    rt.room(roomId, function (object) {
+
+      console.log(object)
+        // 判断服务器端是否存在这个 room，如果存在
+      if (object) {
+        room = object;
+
+        // 当前用户加入这个房间
+        room.join(function () {
+          room.list(function (data) {
+            showLog('当前 Conversation 的成员列表：', data);
+          });
+        });
+
+        // 房间接受消息
+        room.receive(function (data) {
+          // console.log(data);
+          var text = '';
+          if (data.msg.type) {
+            text = data.msg.text;
+          } else {
+            text = data.msg;
+          }
+          showLog(data.fromPeerId + '： ', text);
+        });
+      } else {
+        var room2 = rt.room({
+          // 成员列表
+          members: [
+        'LeanCloud02'
+    ],
+          // 默认的数据，可以放 room 名字等
+          data: {
+            title: 'testTitle'
+          }
+        }, function (result) {
+          console.log('Room created callback');
+        });
+
+        // 当新 Room 被创建时触发
+        rt.on('create', function (data) {
+          console.log(data);
+        });
+      }
+    });
+  });
+
+  // 监听服务情况
+  rt.on('resue', function () {
+    showLog('服务器正在重连，请耐心等待。。。');
+  });
+}
+
+function sendMsg() {
+
+  // 如果没有连接过服务器
+  if (firstFlag) {
+    alert('请先连接服务器！');
+    return;
+  }
+  var val = inputSend.value;
+
+  // 向这个房间发送消息，这段代码是兼容多终端格式的，包括 iOS、Android、Window Phone
+  room.send({
+    text: val
+  }, {
+    type: 'text'
+  }, function (data) {
+
+    // 发送成功之后的回调
+    inputSend.value = '';
+    showLog('自己： ', val);
+    printWall.scrollTop = printWall.scrollHeight;
+  });
+
+  // 发送多媒体消息，如果想测试图片发送，可以打开注释
+  // room.send({
+  //     text: '图片测试',
+  //     // 自定义的属性
+  //     attr: {
+  //         a:123
+  //     },
+  //     url: 'https://leancloud.cn/images/static/press/Logo%20-%20Blue%20Padding.png',
+  //     metaData: {
+  //         name:'logo',
+  //         format:'png',
+  //         height: 123,
+  //         width: 123,
+  //         size: 888
+  //     }
+  // }, {
+  //    type: 'image'
+  // }, function(data) {
+  //     console.log('图片数据发送成功！');
+  // });
+}
+
+// demo 中输出代码
+function showLog(msg, data) {
+  if (data) {
+    // console.log(msg, data);
+    msg = msg + '<span class="strong">' + encodeHTML(JSON.stringify(data)) + '</span>';
+  } else {
+    // console.log(msg);
+  }
+  var p = document.createElement('p');
+  p.innerHTML = msg;
+  printWall.appendChild(p);
+}
+
+function encodeHTML(source) {
+  return String(source)
+    .replace(/&/g, '&amp;')
+    .replace(/</g, '&lt;')
+    .replace(/>/g, '&gt;');
+  // .replace(/\\/g,'&#92;')
+  // .replace(/"/g,'&quot;')
+  // .replace(/'/g,'&#39;');
+}
+
+function bindEvent(dom, eventName, fun) {
+  if (window.addEventListener) {
+    dom.addEventListener(eventName, fun);
+  } else {
+    dom.attachEvent('on' + eventName, fun);
+  }
+}
