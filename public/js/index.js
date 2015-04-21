7c7f8ff5c3e7dfca349a4e49ea363c3dd8caaa07
grepug <sk@grepug.com>
2015-04-21 12:20:14 +0800

    commit my first version

--- public/js/index.js
+++ public/js/index.js
@@ -1,115 +1,67 @@
+var role = 'white';
+var versus = [];
+var myturn = true;
+
 $(function () {
 
-  var role = 'white';
-  var versus = [];
+  var $table = $('table');
   for (var i = 0; i < 15; i++) {
     versus[i] = [];
     for (var j = 0; j < 15; j++)
       versus[i][j] = null;
   }
-  $('table').html(createTable());
-  $(".empty").text('`').on('click', function () {
+  $table.html(createTable()).delegate('.empty', 'click', function () {
+    if (firstFlag) {
+      alert('请先进入房间');
+      return;
+    }
+
+    if (!myturn) {
+      alert('对面还没走，别着急！！！！');
+      return;
+    }
+
     role = role == 'white' ? 'black' : 'white';
     var $this = $(this),
       x = $this.data('x'),
       y = $this.data('y');
     $this.replaceWith("<img class='pawn' data-x='" + x + "' data-y='" + y + "' src='img/" + role + ".jpg'>");
     versus[x][y] = role;
+
     console.log(versus);
-    var win = isWin();
-    if (win) {
-      console.log(role + " win!");
-      console.log(win);
-      for (var i = 0; i < win.length; i++) {
-        $("[data-x='" + win[i][0] + "'][data-y='" + win[i][1] + "']").attr('src', 'img/' + role + '-win.jpg');
-        console.log(win[i][0])
+
+    room.send({
+      currentStep: [x, y],
+      versus: versus,
+      nextRole: role
+    }, function (data) {
+
+      // 发送成功之后的回调
+      myturn = false;
+
+      showLog('自己： ', "x:" + x + " y:" + y);
+      printWall.scrollTop = printWall.scrollHeight;
+
+      var win = isWin();
+      if (win) {
+        console.log(role + " win!");
+        console.log(win);
+        for (var i = 0; i < win.length; i++) {
+          $("[data-x='" + win[i][0] + "'][data-y='" + win[i][1] + "']").attr('src', 'img/' + role + '-win.jpg');
+          console.log(win[i][0])
+        }
+        $('table').undelegate();
       }
-      //location.reload();
-    }
+    });
   });
 
-  function isWin() {
 
-    for (var i = 0; i < 15; i++) {
-      for (var j = 0; j < 11; j++) {
-        var winArr = straightEqual(false, '+', i, j)
-        if (winArr) return winArr;
-      }
-    }
+  $(".empty").text('`');
 
-    for (var i = 0; i < 15; i++) {
-      for (var j = 0; j < 11; j++) {
-        var winArr = straightEqual('+', false, i, j)
-        if (winArr) return winArr;
-      }
-    }
 
-    for (var i = 0; i < 14; i++) {
-
-      for (var j = 0; j < 14; j++) {
-
-        if (i <= 10 && j <= 10) {
-          var winArr = straightEqual('+', '+', i, j)
-          if (winArr) return winArr;
-        }
-        if (i >= 4 && j >= 4) {
-          var winArr = straightEqual('-', '-', i, j)
-          if (winArr) return winArr;
-        }
-        if (i >= 4 && j <= 10) {
-          var winArr = straightEqual('-', '+', i, j)
-          if (winArr) return winArr;
-        }
-        if (i <= 10 && j >= 4) {
-          var winArr = straightEqual('+', '-', i, j)
-          if (winArr) return winArr;
-        }
-      }
-    }
-    return false;
-  }
-
-  function straightEqual(m, m1, _i, _j) {
-    if (versus[_i][_j] == null) return false;
-    var winArr = [[_i, _j]];
-    for (var i = 1; i <= 4; i++) {
-
-      if (!m && m1 == '+') {
-        if (versus[_i][_j] != versus[_i][_j + i]) return false;
-        winArr.push([_i, _j + i]);
-      }
-      if (!m && m1 == '-') {
-        if (versus[_i][_j] != versus[_i][_j - i]) return false;
-        winArr.push([_i, _j - i]);
-      }
-      if (m == '+' && !m1) {
-        if (versus[_i][_j] != versus[_i + i][_j]) return false;
-        winArr.push([_i + i, _j]);
-      }
-      if (m == '+' && m1 == '+') {
-        if (versus[_i][_j] != versus[_i + i][_j + i]) return false;
-        winArr.push([_i + i, _j + i]);
-      }
-      if (m == '+' && m1 == '-') {
-        if (versus[_i][_j] != versus[_i + i][_j - i]) return false;
-        winArr.push([_i + i, _j - i]);
-      }
-      if (m == '-' && !m1) {
-        if (versus[_i][_j] != versus[_i - i][_j]) return false;
-        winArr.push([_i - i, _j]);
-      }
-      if (m == '-' && m1 == '+') {
-        if (versus[_i][_j] != versus[_i - i][_j + i]) return false;
-        winArr.push([_i - i, _j + i]);
-      }
-      if (m == '-' && m1 == '-') {
-        if (versus[_i][_j] != versus[_i - i][_j - i]) return false;
-        winArr.push([_i - i, _j - i]);
-      }
-
-    }
-    return winArr;
-  }
+  //  $(window).bind('beforeunload', function () {
+  //    return '您输入的内容尚未保存，确定离开此页面吗？';
+  //  });
 });
 
 function createTable() {
@@ -122,4 +74,87 @@ function createTable() {
     ret += '</tr>';
   }
   return ret;
+}
+
+function isWin() {
+  //横着赢
+  for (var i = 0; i < 15; i++) {
+    for (var j = 0; j < 15; j++) {
+      var winArr = straightEqual(false, '+', i, j)
+      if (winArr) return winArr;
+    }
+  }
+  //竖着赢
+  for (var i = 0; i < 15; i++) {
+    for (var j = 0; j < 15; j++) {
+      var winArr = straightEqual('+', false, i, j)
+      if (winArr) return winArr;
+    }
+  }
+  //躺着赢
+  for (var i = 0; i < 14; i++) {
+
+    for (var j = 0; j < 14; j++) {
+
+      if (i <= 10 && j <= 10) {
+        var winArr = straightEqual('+', '+', i, j);
+        if (winArr) return winArr;
+      }
+      if (i >= 4 && j >= 4) {
+        var winArr = straightEqual('-', '-', i, j);
+        if (winArr) return winArr;
+      }
+      if (i >= 4 && j <= 10) {
+        var winArr = straightEqual('-', '+', i, j);
+        if (winArr) return winArr;
+      }
+      if (i <= 10 && j >= 4) {
+        var winArr = straightEqual('+', '-', i, j);
+        if (winArr) return winArr;
+      }
+    }
+  }
+  return false;
+}
+
+function straightEqual(m, m1, _i, _j) {
+  if (versus[_i][_j] == null) return false;
+  var winArr = [[_i, _j]];
+  for (var i = 1; i <= 4; i++) {
+
+    if (!m && m1 == '+') {
+      if (versus[_i][_j] != versus[_i][_j + i]) return false;
+      winArr.push([_i, _j + i]);
+    }
+    if (!m && m1 == '-') {
+      if (versus[_i][_j] != versus[_i][_j - i]) return false;
+      winArr.push([_i, _j - i]);
+    }
+    if (m == '+' && !m1) {
+      if (versus[_i][_j] != versus[_i + i][_j]) return false;
+      winArr.push([_i + i, _j]);
+    }
+    if (m == '+' && m1 == '+') {
+      if (versus[_i][_j] != versus[_i + i][_j + i]) return false;
+      winArr.push([_i + i, _j + i]);
+    }
+    if (m == '+' && m1 == '-') {
+      if (versus[_i][_j] != versus[_i + i][_j - i]) return false;
+      winArr.push([_i + i, _j - i]);
+    }
+    if (m == '-' && !m1) {
+      if (versus[_i][_j] != versus[_i - i][_j]) return false;
+      winArr.push([_i - i, _j]);
+    }
+    if (m == '-' && m1 == '+') {
+      if (versus[_i][_j] != versus[_i - i][_j + i]) return false;
+      winArr.push([_i - i, _j + i]);
+    }
+    if (m == '-' && m1 == '-') {
+      if (versus[_i][_j] != versus[_i - i][_j - i]) return false;
+      winArr.push([_i - i, _j - i]);
+    }
+
+  }
+  return winArr;
 }
--- public/js/realtime.js
+++ public/js/realtime.js
@@ -1,8 +1,11 @@
 // 请将 AppId 改为你自己的 AppId，否则无法本地测试
 var appId = '3l36nhyvwa2gg80zqq14lkkyhmmx1ukt7t89yg0byzyfqv4m';
 
+
+//var Conv = AV.Object.extend('Conversation');
+
 // 请换成你自己的一个房间的 conversation id
-var roomId = '55351818e4b0825685f6d7b8';
+var roomId = '5535a578e4b052897f517ede';
 
 // 每个客户端自定义的 id
 var clientId = 'mygobang';
@@ -18,9 +21,11 @@ var sendBtn = document.getElementById('send-btn');
 var inputName = document.getElementById('input-name');
 var inputSend = document.getElementById('input-send');
 var printWall = document.getElementById('print-wall');
+//var quitBtn = document.getElementById('quit-btn');
 
 bindEvent(openBtn, 'click', main);
 bindEvent(sendBtn, 'click', sendMsg);
+//bindEvent(quitBtn, 'click', quit);
 
 bindEvent(document.body, 'keydown', function (e) {
   if (e.keyCode === 13) {
@@ -31,29 +36,31 @@ bindEvent(document.body, 'keydown', function (e) {
     }
   }
 });
+$(function () {
+
+})
 
 function main() {
-  showLog('正在链接服务器，请等待。。。');
-  var val = inputName.value;
-  if (val) {
-    clientId = val;
-  }
-  if (!firstFlag) {
-    rt.close();
-  }
 
-  // 创建实时通信实例
+  showLog('正在连接服务器，请等待。。。');
+  var val = inputName.value;
+  if (val) clientId = val;
+  firstFlag || rt.close();
+
+  console.log(clientId)
+    // 创建实时通信实例
   rt = AV.realtime({
     appId: appId,
     clientId: clientId
   });
 
+  rt.on('left', function (data) {
+    console.log(data);
+  });
   // 监听连接成功事件
   rt.on('open', function () {
     firstFlag = false;
     showLog('服务器连接成功！');
-
-    // 获得已有房间的实例
     rt.room(roomId, function (object) {
 
       console.log(object)
@@ -63,48 +70,80 @@ function main() {
 
         // 当前用户加入这个房间
         room.join(function () {
-          room.list(function (data) {
-            showLog('当前 Conversation 的成员列表：', data);
+
+          room.count(function (data) {
+            // 当前用户数量
+            if (data > 2) {
+              room.leave();
+              showLog('该房间已满，已离开');
+              return;
+            } else {
+              room.list(function (data) {
+                showLog('当前 Conversation 的成员列表：', data);
+              });
+            }
           });
         });
 
         // 房间接受消息
         room.receive(function (data) {
-          // console.log(data);
+          console.log(data);
           var text = '';
           if (data.msg.type) {
             text = data.msg.text;
+          } else if (data.msg.currentStep) {
+            var x = data.msg.currentStep[0],
+              y = data.msg.currentStep[1];
+            $("[data-x='" + x + "'][data-y='" + y + "']").replaceWith("<img class='pawn' data-x='" + x + "' data-y='" + y + "' src='img/" + data.msg.nextRole + ".jpg'>");
+            versus = data.msg.versus;
+            role = data.msg.nextRole;
+            myturn = true;
+            console.log(versus);
+            var win = isWin();
+            if (win) {
+              console.log(role + " win!");
+              console.log(win);
+              for (var i = 0; i < win.length; i++) {
+                $("[data-x='" + win[i][0] + "'][data-y='" + win[i][1] + "']").attr('src', 'img/' + role + '-win.jpg');
+                console.log(win[i][0])
+              }
+              $('table').undelegate();
+            }
           } else {
             text = data.msg;
           }
           showLog(data.fromPeerId + '： ', text);
         });
-      } else {
-        var room2 = rt.room({
-          // 成员列表
-          members: [
-        'LeanCloud02'
-    ],
-          // 默认的数据，可以放 room 名字等
-          data: {
-            title: 'testTitle'
-          }
-        }, function (result) {
-          console.log('Room created callback');
-        });
-
-        // 当新 Room 被创建时触发
-        rt.on('create', function (data) {
-          console.log(data);
-        });
+        //    } else {
+        //      var room2 = rt.room({
+        //        // 成员列表
+        //        members: [
+        //        'LeanCloud02'
+        //    ],
+        //        // 默认的数据，可以放 room 名字等
+        //        data: {
+        //          title: 'testTitle'
+        //        }
+        //      }, function (result) {
+        //        console.log('Room created callback');
+        //      });
+        //
+        //      // 当新 Room 被创建时触发
+        //      rt.on('create', function (data) {
+        //        console.log(data);
+        //      });
+        //    }
       }
     });
   });
-
-  // 监听服务情况
   rt.on('resue', function () {
     showLog('服务器正在重连，请耐心等待。。。');
   });
+  // 获得已有房间的实例
+
+
+  // 监听服务情况
+
 }
 
 function sendMsg() {
@@ -149,6 +188,15 @@ function sendMsg() {
   // }, function(data) {
   //     console.log('图片数据发送成功！');
   // });
+}
+
+function quit() {
+  if (firstFlag) {
+    alert('请先进入房间');
+    return;
+  }
+  room.leave();
+
 }
 
 // demo 中输出代码
